1b89b3d5d9bc49bb451ac70acbc4d0f7ba3e31b8
Prakash-Balu <prakash.engg89@gmail.com>
2018-12-19 23:38:25 +0530

    dashboard chart changes

--- application/models/login/Login_model.php
+++ application/models/login/Login_model.php
@@ -44,6 +44,19 @@ Class Login_model extends CI_Model {
         
         return $query->result_array();
     }
+
+    function getDeviceList( $username ) {
+        $result = array();
+        if (empty($username)) {
+            return $result;
+        }
+
+        $this->db->select('username, device_type')
+                ->where('username',$username);
+        $query = $this->db->get('device_master');
+        
+        return array_column($query->result_array(), 'device_type');
+    }
 	
 	 function specialCharacterRemoval( $string ) {
        return preg_replace('#[^\w()/.%\-]#'," ", $string);
